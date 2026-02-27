0b669e96dc7f1d16ea6489e9e3835d74f864c2af
Sanzhar Bishmanov <bishmanov@gmail.com>
2026-02-27 23:33:22 +0500

    Refactor setEnv method to remove unused env variables
    Removed environment variable settings in setEnv method.

--- tests/Hitrov/Test/OciApiWithEnvFileTest.php
+++ tests/Hitrov/Test/OciApiWithEnvFileTest.php
@@ -19,13 +19,6 @@ class OciApiWithEnvFileTest extends OciApiTest
 
     protected function setEnv(): void
     {
-        putenv('OCI_SHAPE');
-        putenv('OCI_OCPUS');
-        putenv('OCI_MEMORY_IN_GBS');
-        putenv('OCI_AVAILABILITY_DOMAIN');
-        putenv('OCI_IMAGE_ID');
-        putenv('OCI_SUBNET_ID');
-
-        $this->loadEnv();
+        
     }
 }
